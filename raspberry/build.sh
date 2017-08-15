123f91a06f536c1c47ceef85cc6c4b56891aab46
Jose Maria <melopsitaco@gmail.com>
2017-08-15 20:34:04 +0200

    Raspberry pi support

--- raspberry/build.sh
+++
@@ -1,8 +0,0 @@
-
-wget https://github.com/opencv/opencv/archive/3.2.0.zip
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D WITH_V4L=ON -DBUILD_QOPENTLD=ON -DUSE_SYSTEM_LIBS=OFF -D BUILD_PYTHON_SUPPORT=ON -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D CMAKE_INSTALL_PREFIX=/usr/local ..
-make -j
-
-
